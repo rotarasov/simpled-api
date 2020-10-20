a033d12fb014dfa03105e522ced04aa45770b895
Roman Tarasov <portafortuna12@outlook.com>
2020-10-20 16:57:24 +0300

    Configured settings for development and production version

--- simpled/wsgi.py
+++ simpled/wsgi.py
@@ -3,12 +3,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 
-if os.environ.get('LOCAL_DEVELOPMENT'):
-    settings_path = 'simpled.local_settings'
-else:
-    settings_path = 'simpled.production_settings'
-
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_path)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'simpled.settings')
 
 application = get_wsgi_application()
