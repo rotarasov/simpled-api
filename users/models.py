17923ee4f6e26ee46bbf72967245724f03e57c88
Roman Tarasov <portafortuna12@outlook.com>
2020-10-22 16:03:08 +0300

    Created users model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+from .managers import UserManager
+
+
+class User(AbstractUser):
+    image = models.ImageField(_('profile image'), upload_to='profile_pics', blank=True)
+    username = None
+    email = models.EmailField(_('email address'), unique=True)
+    bio = models.TextField(_('biography'), blank=True)
+    first_name = models.CharField(_('first name'), max_length=32)
+    last_name = models.CharField(_('last name'), max_length=32)
+    points = models.PositiveIntegerField(_('points'), default=0)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
