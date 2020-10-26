87a89422cdea89685733a29fb67f3e6dbba89850
Roman Tarasov <portafortuna12@outlook.com>
2020-10-26 12:25:44 +0200

    Created endpoints for user CRUD

--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework.generics import RetrieveUpdateDestroyAPIView, CreateAPIView
 
-# Create your views here.
+from .serializers import UserSerializer
+
+User = get_user_model()
+
+class UserCreateAPIView(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserReadUpdateDeleteAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
