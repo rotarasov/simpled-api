4b209f60a041d74f3509a5f75221fd84742a20ab
Roman Tarasov <portafortuna12@outlook.com>
2020-10-27 22:18:37 +0200

    Changed language field to CharField course model

--- courses/models.py
+++ courses/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-from languages.fields import LanguageField
 
 from simpled import settings
 
 # TODO add category
 class Course(models.Model):
+    class Languages(models.TextChoices):
+        """
+            Short alpha-2 code taken from ISO 639-1
+        """
+        Russian = 'ru', _('Russian')
+        English = 'en', _('English')
+        Chinese = 'zh', _('Chinese')
+        Spanish = 'es', _('Spanish')
+        French = 'fr', _('French')
+
     class Categories(models.TextChoices):
         pass
 
@@ -15,7 +24,7 @@ class Course(models.Model):
     title = models.CharField(_('title'), max_length=100, unique=True)
     description = models.TextField(_('description'), blank=True)
     category = models.CharField(_('category'), max_length=20, choices=Categories.choices)
-    language = LanguageField(_('language'))
+    language = models.CharField(_('language'), max_length=3, choices=Languages.choices)
     start_date = models.DateField(_('start date'), auto_now_add=True)
     participants = models.ManyToManyField(settings.AUTH_USER_MODEL, db_table='participation',
                                           related_name='attended_courses', related_query_name='attended_course')
