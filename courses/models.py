8ffd364fd839270a0ca63ed338e7597b9c333c69
Roman Tarasov <portafortuna12@outlook.com>
2020-10-27 21:51:16 +0200

    Created course model

--- courses/models.py
+++ courses/models.py
@@ -1,25 +1,35 @@
-import json
-
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+from languages.fields import LanguageField
 
-from simpled.settings import BASE_DIR
+from simpled import settings
 
-# TODO add category and language choices
+# TODO add category
 class Course(models.Model):
     class Categories(models.TextChoices):
         pass
 
-    class Languages(models.TextChoices):
-        pass
-
+    image = models.ImageField(_('image'), upload_to='course_pics', default='default.png')
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+                                related_name='created_courses', related_query_name='created_course')
     title = models.CharField(_('title'), max_length=100, unique=True)
     description = models.TextField(_('description'), blank=True)
-    category = models.CharField(_('category'), choices=Categories.choices)
-    language = models.CharField(_('language'), choices=Languages.choices)
+    category = models.CharField(_('category'), max_length=20, choices=Categories.choices)
+    language = LanguageField(_('language'))
+    start_date = models.DateField(_('start date'), auto_now_add=True)
+    participants = models.ManyToManyField(settings.AUTH_USER_MODEL, db_table='participation',
+                                          related_name='attended_courses', related_query_name='attended_course')
 
-    # @classmethod
-    # def get_all_categories(self):
-    #     path = BASE_DIR('choices.json')
-    #     choices = json.load(path)
-    #     return []
+    def __str__(self):
+        return self.title
+
+
+# class Task(models.Model):
+#
+#     title = models.CharField(_('title'), max_length=100)
+#     description = None
+#     course = models.ForeignKey('Course', on_delete=models.CASCADE)
+#
+#     def __str__(self):
+#         return self.title
+
