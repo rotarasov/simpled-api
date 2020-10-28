c32383e8de89af9a08bde55275196188f5e6bddc
Roman Tarasov <portafortuna12@outlook.com>
2020-10-28 14:58:12 +0200

    Created enpdoints for courses CRUD operations

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView
 
-# Create your views here.
+from .models import Course
+from .serializers import CourseSerializer
+
+
+class CourseCreateAPIView(CreateAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
+class CourseReadUpdateDeleteAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
