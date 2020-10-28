fe1b3931d72dcf4f22237b562e9d49e5cc7d811f
Roman Tarasov <portafortuna12@outlook.com>
2020-10-28 21:37:03 +0200

    Deleted blank parameter from image fields in course and user models

--- users/models.py
+++ users/models.py
@@ -6,8 +6,7 @@ from .managers import UserManager
 
 
 class User(AbstractUser):
-    image = models.ImageField(_('profile image'), upload_to='profile_pics', default='profile_pics/default.jpg',
-                              blank=True)
+    image = models.ImageField(_('profile image'), upload_to='profile_pics', default='profile_pics/default.jpg')
     username = None
     email = models.EmailField(_('email address'), unique=True)
     bio = models.TextField(_('biography'), blank=True)
