dd34f02bca0e8f316cf969a2904cf8dddbc8027e
Roman Tarasov <portafortuna12@outlook.com>
2020-10-28 16:51:20 +0200

    Added redoc to generate API docs

--- simpled/urls.py
+++ simpled/urls.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="SimplED API",
+      default_version='v1',
+      description="Good Luck, mobile developers :)",
+      contact=openapi.Contact(email="roman.tarasov1@nure.ua"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
-    path('courses/', include('courses.urls'))
+    path('courses/', include('courses.urls')),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc-ui')
 ]
