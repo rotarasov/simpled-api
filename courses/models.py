b41bf6d648c5a90e1c95a357425b0b02969ac3ef
Roman Tarasov <portafortuna12@outlook.com>
2020-10-28 11:03:11 +0200

    Added test categories and TODO comment

--- courses/models.py
+++ courses/models.py
@@ -4,6 +4,7 @@ from django.utils.translation import ugettext_lazy as _
 from simpled import settings
 
 # TODO add category
+# TODO add default for creator field to match current authorized user
 class Course(models.Model):
     class Languages(models.TextChoices):
         """
@@ -16,14 +17,14 @@ class Course(models.Model):
         French = 'fr', _('French')
 
     class Categories(models.TextChoices):
-        pass
+        PROGRAMMING = 'programming', _('Programming')
 
     image = models.ImageField(_('image'), upload_to='course_pics', default='default.png')
     creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
                                 related_name='created_courses', related_query_name='created_course')
     title = models.CharField(_('title'), max_length=100, unique=True)
     description = models.TextField(_('description'), blank=True)
-    category = models.CharField(_('category'), max_length=20, choices=Categories.choices)
+    category = models.CharField(_('category'), max_length=50, choices=Categories.choices)
     language = models.CharField(_('language'), max_length=3, choices=Languages.choices)
     start_date = models.DateField(_('start date'), auto_now_add=True)
     participants = models.ManyToManyField(settings.AUTH_USER_MODEL, db_table='participation',
