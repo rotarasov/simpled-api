603bca6672884ae7c241590bb28ebc34d177251d
Roman Tarasov <portafortuna12@outlook.com>
2020-10-28 15:41:11 +0200

    Changed plug for creator field in CourseSerializer

--- courses/serializers.py
+++ courses/serializers.py
@@ -6,10 +6,9 @@ from users.models import User
 class CourseSerializer(serializers.ModelSerializer):
     # Uncomment when authorisation is provided
     # creator = serializers.PrimaryKeyRelatedField(read_only=True, default=serializers.CurrentUserDefault())
-    creator = serializers.PrimaryKeyRelatedField(read_only=True, default=User.objects.first()) # REMOVE LATER
+    creator = serializers.PrimaryKeyRelatedField(queryset=User.objects.all()) # REMOVE LATER
     participants = serializers.PrimaryKeyRelatedField(read_only=True, many=True)
 
     class Meta:
         model = Course
         fields = '__all__'
-        read_only_fields = ['creator']
