3c279d2f826dbb4a25dd33658c956ced6166878e
Roman Tarasov <portafortuna12@outlook.com>
2020-10-28 21:39:29 +0200

    Added static and media url for debug mode

--- simpled/urls.py
+++ simpled/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+
+from simpled import settings
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -21,3 +24,7 @@ urlpatterns = [
     path('courses/', include('courses.urls')),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc-ui')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
