01cabb53ed5f99c2fba470c38e43cc59d13cd418
Roman Tarasov <portafortuna12@outlook.com>
2020-10-28 17:48:47 +0200

    Added url to ALLOWED_HOST in django settings

--- simpled/settings/django.py
+++ simpled/settings/django.py
@@ -16,7 +16,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['simpled-api.herokuapp.com']
 
 
 # Application definition
