19aea08ebac66a2ffbc7006f63fa13d587b7527c
Roman Tarasov <portafortuna12@outlook.com>
2020-10-29 23:21:27 +0200

    Added utils endpoints for listing all course categories and languages

--- courses/views.py
+++ courses/views.py
@@ -1,7 +1,29 @@
 from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from .models import Course
 from .serializers import CourseSerializer
+
+
+@api_view(http_method_names=['GET'])
+def get_all_categories(request):
+    categories = {
+        'categories': [
+            {'db value': category[0], 'human-readable name': category[1]} for category in Course.Categories.choices
+        ]
+    }
+    return Response(categories)
+
+
+@api_view(http_method_names=['GET'])
+def get_all_languages(request):
+    languages = {
+        'languages': [
+            {'db value': language[0], 'human-readable name': language[1]} for language in Course.Languages.choices
+        ]
+    }
+    return Response(languages)
 
 
 class CourseCreateAPIView(CreateAPIView):
