7072b364dcbc59737325f1e2b1fd5bf961725bff
Roman Tarasov <portafortuna12@outlook.com>
2020-10-29 23:31:05 +0200

    Added util functions to course urls

--- courses/urls.py
+++ courses/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('<int:pk>', views.CourseReadUpdateDeleteAPIView.as_view(), name='course-detail'),
     path('', views.CourseCreateAPIView.as_view(), name='course-create'),
+    path('categories/', views.get_all_categories, name='course-categories'),
+    path('languages/', views.get_all_languages, name='course-languages')
 ]
