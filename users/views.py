ab1afde899eeb2e0f4827870116877e6f6437e93
Roman Tarasov <portafortuna12@outlook.com>
2020-10-29 23:25:40 +0200

    Added TODO

--- users/views.py
+++ users/views.py
@@ -3,7 +3,10 @@ from rest_framework.generics import RetrieveUpdateDestroyAPIView, CreateAPIView
 
 from .serializers import UserSerializer
 
+
 User = get_user_model()
+
+# TODO: create post delete hook to delete picture from cloudinary
 
 class UserCreateAPIView(CreateAPIView):
     queryset = User.objects.all()
