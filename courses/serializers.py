24b38801116752c18b18373d630213f9ac9e8ec1
Roman Tarasov <portafortuna12@outlook.com>
2020-10-30 13:05:07 +0200

    Added nested serialization for creator field in case of GET request

--- courses/serializers.py
+++ courses/serializers.py
@@ -1,14 +1,30 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 from .models import Course
-from users.models import User
+from users.serializers import UserSerializer
+
+
+User = get_user_model()
+
 
 class CourseSerializer(serializers.ModelSerializer):
     # Uncomment when authorisation is provided
     # creator = serializers.PrimaryKeyRelatedField(read_only=True, default=serializers.CurrentUserDefault())
-    creator = serializers.PrimaryKeyRelatedField(queryset=User.objects.all()) # REMOVE LATER
-    participants = serializers.PrimaryKeyRelatedField(read_only=True, many=True)
+    creator = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+    participants = UserSerializer(read_only=True, many=True)
 
     class Meta:
         model = Course
         fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        request = kwargs.get('context', {}).get('request')
+
+        super().__init__(*args, **kwargs)
+
+        if request and request.method == 'GET':
+            self.fields['creator'] = UserSerializer()
+
+
+
