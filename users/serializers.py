5852eceb59376dd494f9217dd85dd4334cd2fd6d
Roman Tarasov <portafortuna12@outlook.com>
2020-11-13 16:02:35 +0200

    Added different serializers for creation, update and change password

--- users/serializers.py
+++ users/serializers.py
@@ -1,15 +1,18 @@
 from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 
+User = get_user_model()
+
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = get_user_model()
-        fields = ['id', 'image', 'email', 'first_name', 'last_name', 'bio', 'points', 'password']
+        model = User
+        fields = ['id', 'image', 'email', 'first_name', 'last_name', 'bio', 'points']
         read_only_fields = ['points']
-        extra_kwargs = {'password': {'write_only': True}}
 
 
 class UserReadSerializer(UserSerializer):
@@ -25,20 +28,55 @@ class UserReadSerializer(UserSerializer):
             self.fields['enrolled_courses'] = CourseSerializer(read_only=True, many=True, source='get_enrolled_courses')
 
 
-class UserCreateUpdateSerializer(UserSerializer):
-    confirm_password = serializers.CharField(write_only=True)
+class UserCreateSerializer(UserSerializer):
+    confirm_password = serializers.CharField(write_only=True, required=True)
 
     class Meta(UserSerializer.Meta):
-        fields = UserSerializer.Meta.fields + ['confirm_password']
+        fields = UserSerializer.Meta.fields + ['password', 'confirm_password']
+        extra_kwargs = {'password': {'write_only': True}}
 
     def validate(self, attrs):
         password = attrs.get('password', None)
         confirm_password = attrs.pop('confirm_password', None)
 
         if not password or password != confirm_password:
-            raise ValidationError('password and confirm_password must be equal')
+            raise ValidationError(_('password and confirm_password must be equal'))
 
         return attrs
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
+
+
+class UserUpdatePasswordSerializer(serializers.Serializer):
+    old_password = serializers.CharField(write_only=True, required=True)
+    new_password = serializers.CharField(write_only=True, required=True)
+    confirm_password = serializers.CharField(write_only=True, required=True)
+
+    def validate_old_password(self, value):
+        user = self.context.get('request').user
+
+        if not user.check_password(value):
+            raise ValidationError(_('Wrong password'))
+
+        return value
+
+    def validate(self, attrs):
+        old_password = attrs.get('old_password')
+        new_password = attrs.get('new_password')
+
+        if old_password == new_password:
+            raise ValidationError(_('Passwords can not be equal'))
+
+        return attrs
+
+    def save(self, **kwargs):
+        new_password = self.validated_data.get('new_password')
+        user = self.context.get('request').user
+        user.set_password(new_password)
+        user.save()
+        return user
+
 
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
