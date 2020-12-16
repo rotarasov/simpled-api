2134cca527634bbe32e2e8e4ee0a0d95922ebb57
Roman Tarasov <portafortuna12@outlook.com>
2020-12-17 00:05:36 +0200

    Fixed bugs in third party settings

--- simpled/settings/third_party.py
+++ simpled/settings/third_party.py
@@ -4,7 +4,7 @@ REST_FRAMEWORK = {
     ),
 
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated'
+        'rest_framework.permissions.IsAuthenticated',
     )
 }
 
