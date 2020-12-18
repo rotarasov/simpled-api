06edb3099e418f4514758eddd54719ebb2030fc1
Roman Tarasov <portafortuna12@outlook.com>
2020-12-18 06:19:27 +0200

    Added nested user in message serializer

--- chats/serializers.py
+++ chats/serializers.py
@@ -1,9 +1,16 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 from chats.models import Message
+from users.serializers import UserSerializer
+
+
+User = get_user_model()
 
 
 class MessageSerializer(serializers.ModelSerializer):
+    sender = UserSerializer(queryset=User.objects.all())
+
     class Meta:
         model = Message
         fields = '__all__'
