7db0d766d49070c628f4e881021e44cc4094d60c
Roman Tarasov <portafortuna12@outlook.com>
2020-12-18 05:32:11 +0200

    Added default value for timestamp

--- chats/models.py
+++ chats/models.py
@@ -8,7 +8,7 @@ class Message(models.Model):
     course = models.ForeignKey('courses.Course', on_delete=models.CASCADE, related_name='messages')
     sender = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='messages')
     text = models.CharField(_('text'), max_length=512)
-    timestamp = models.DateTimeField(_('timestamp'))
+    timestamp = models.DateTimeField(_('timestamp'), auto_now_add=True)
 
     class Meta:
         ordering = ('timestamp',)
