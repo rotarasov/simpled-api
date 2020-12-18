5d7e89d78c277cf3b5d21e98378e3be590669f2d
Roman Tarasov <portafortuna12@outlook.com>
2020-12-18 07:55:25 +0200

    Added endpoint for user notification

--- users/urls.py
+++ users/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('token/', views.CustomTokenObtainPairAPIView.as_view(), name='token-obtain-pair'),
     path('refresh/', TokenRefreshView.as_view(), name='token-refresh'),
     path('<int:pk>/change-password/', views.update_user_password, name='change-password'),
-    path('video-chat-users/', views.get_all_user_video_chat_profiles, name='video-chat-user-list')
+    path('video-chat-users/', views.get_all_user_video_chat_profiles, name='video-chat-user-list'),
+    path('notify/', views.NotifyUsersAPIView.as_view(), name='notify-users')
 ]
--- users/views.py
+++ users/views.py
@@ -1,8 +1,13 @@
+from smtplib import SMTPException
+
 from django.contrib.auth import get_user_model
+from django.http import Http404
+from rest_framework import status
 from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListCreateAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from .serializers import (
@@ -12,7 +17,7 @@ from .serializers import (
     CustomTokenObtainPairSerializer,
     UserUpdatePasswordSerializer,
     UserForVideoChatSerializer)
-from .services import get_users_for_video_chat_display
+from .services import get_users_for_video_chat_display, notify_users
 
 User = get_user_model()
 
@@ -54,3 +59,12 @@ class UserReadUpdateDeleteAPIView(RetrieveUpdateDestroyAPIView):
 class CustomTokenObtainPairAPIView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
     permission_classes = [AllowAny]
+
+
+class NotifyUsersAPIView(APIView):
+    def post(self, request, *args, **kwargs):
+        try:
+            notify_users(request.data['users'], request.data['subject'], request.data['message'])
+        except SMTPException:
+            raise Http404
+        return Response(status=status.HTTP_204_NO_CONTENT)
