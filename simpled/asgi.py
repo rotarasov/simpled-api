884108528cf1a3d2377e19c341d1100316367051
Roman Tarasov <portafortuna12@outlook.com>
2020-12-18 03:17:46 +0200

    Changed import order in asgi.py

--- simpled/asgi.py
+++ simpled/asgi.py
@@ -4,11 +4,11 @@ from django.urls import re_path
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 
-from chats.consumers import AsyncChatConsumer
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'simpled.settings')
 
 http_application = get_asgi_application()
+
+from chats.consumers import AsyncChatConsumer
 
 application = ProtocolTypeRouter({
     "http": http_application,
