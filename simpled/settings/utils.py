9d53f3b7ca72493ab5f2a2c58db997310ffd6702
Roman Tarasov <portafortuna12@outlook.com>
2020-12-18 02:49:51 +0200

    Fixed utils

--- simpled/settings/utils.py
+++ simpled/settings/utils.py
@@ -1,6 +1,6 @@
 import re
 
-import cloudinary
+from cloudinary import config as media_storage
 from urllib.parse import urlparse
 
 from . import configuration
@@ -9,4 +9,4 @@ from . import configuration
 def config_media_storage(**options):
     netloc = urlparse(configuration.env.str('CLOUDINARY_URL')).netloc
     api_key, api_secret, cloud_name = re.split('[@:]', netloc)
-    cloudinary.config(cloud_name=cloud_name,api_key=api_key, api_secret=api_secret, **options)
+    media_storage(cloud_name=cloud_name,api_key=api_key, api_secret=api_secret, **options)
