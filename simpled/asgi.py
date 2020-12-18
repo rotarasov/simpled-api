97741aad1dbf56f7f58985783d91435ed019f7e6
Roman Tarasov <portafortuna12@outlook.com>
2020-12-18 03:45:35 +0200

    Configured asgi.py

--- simpled/asgi.py
+++ simpled/asgi.py
@@ -3,5 +3,16 @@ import os
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'simpled.settings')
+django_asgi_app = get_asgi_application()
 
-application = get_asgi_application()
+from django.urls import re_path
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+from chats.consumers import AsyncChatConsumer
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": URLRouter([
+        re_path('chat/<int:course_pk>/', AsyncChatConsumer.as_asgi(), name='chat')
+    ])
+})
